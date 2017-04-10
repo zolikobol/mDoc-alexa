ce8b8c070c1fce731f4cead2b8a741821bd24ac8
zolikobol <zoltan.kobol@globallogic.com>
2017-04-10 20:23:19 +0200

    added all intents from diagram, defined schema and demo utterances

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    /* eslint-disable  func-names */
+/* eslint-disable  func-names */
 /* eslint quote-props: ["error", "consistent"]*/
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills
@@ -15,20 +15,49 @@ const moment = require('moment');
 
 const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
-var skillTexts = {
-    'medicineOffer' : 'You need to take your medicine. When did you take your medicine?'
-};
-
 var dummyValues = {
-    'medicineTime' : '09:00'
+    'medicineTime': '09:00'
 }
+
+var languageStrings = {
+    'en-GB': {
+        'translation': {
+            'MEDICINE_OFFER': 'You need to take your medicine. When did you take your medicine?',
+            'WELCOME_MESSAGE': 'Good morning, who is my pacient today?',
+            'NOT_TAKEN_MEDICINE': 'This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?',
+            'MEDICINE_TAKEN': 'Good to hear. You have to attend doctors visit. When did you attend the visit?',
+            'HELP_MESSAGE': 'Help!',
+            'GOOD_BYE': 'Good bye!'
+        }
+    },
+    'en-US': {
+        'translation': {
+            'MEDICINE_OFFER': 'You need to take your medicine. When did you take your medicine?',
+            'WELCOME_MESSAGE': 'Good morning, who is my pacient today?',
+            'NOT_TAKEN_MEDICINE': 'This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?',
+            'MEDICINE_TAKEN': 'Good to hear. You have to attend doctors visit. When did you attend the visit?',
+            'HELP_MESSAGE': 'Help!',
+            'GOOD_BYE': 'Good bye!'
+        }
+    },
+    'de-DE': {
+        'translation': {
+            'MEDICINE_OFFER': 'You need to take your medicine. When did you take your medicine?',
+            'WELCOME_MESSAGE': 'Good morning, who is my pacient today?',
+            'NOT_TAKEN_MEDICINE': 'This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?',
+            'MEDICINE_TAKEN': 'Good to hear. You have to attend doctors visit. When did you attend the visit?',
+            'HELP_MESSAGE': 'Help!',
+            'GOOD_BYE': 'Good bye!'
+        }
+    }
+};
 
 const handlers = {
     'LaunchRequest': function () {
-        this.emit(':ask' , "Good morning, who is my pacient today?");
+        this.emit(':ask', this.t('WELCOME_MESSAGE'));
     },
     'PatientDefineIntent': function () {
-        if(typeof this.attributes['mdoc'] == 'undefined'){
+        if (typeof this.attributes['mdoc'] == 'undefined') {
             var attributes = this.attributes['mdoc'];
         } else {
             var attributes = {};
@@ -36,38 +65,79 @@ const handlers = {
         var firstName = this.event.request.intent.slots.firstName.value;
         var lastName = this.event.request.intent.slots.lastName.value;
         attributes = {
-            'firstName' : firstName,
-            'lastName' : lastName
+            'firstName': firstName,
+            'lastName': lastName
         }
         this.attributes['mdoc'] = attributes;
-        this.emit(':ask' , "Hello " + firstName + " " + lastName + ". " + skillTexts['medicineOffer'] , skillTexts['medicineOffer']);
+        this.emit(':ask', "Hello " + firstName + " " + lastName + ". " + this.t('MEDICINE_OFFER'), this.t('MEDICINE_OFFER'));
     },
-    'MedicineIntent' : function () {
+    'MedicineIntent': function () {
         var attributes = this.event.session.attributes;
-        var patientMedicineTime = moment(this.event.request.intent.slots.medicineTime.value , 'HH:mm');
-        if(typeof patientMedicineTime == 'undefined'){
-            this.emit(':ask' , "This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?");
+        var patientMedicineTime = moment(this.event.request.intent.slots.medicineTime.value, 'HH:mm');
+        if (typeof patientMedicineTime == 'undefined') {
+            this.emit(':ask', this.t('NOT_TAKEN_MEDICINE'));
         }
-        var medicineTime = moment(dummyValues['medicineTime'] , 'HH:mm');
-        var diff = Math.abs(medicineTime.diff(patientMedicineTime , 'seconds'));
-        if(diff <= 3600){
-            this.emit(':ask' , "Good to hear. You have to attend doctors visit. When did you attend the visit?");
+        var medicineTime = moment(dummyValues['medicineTime'], 'HH:mm');
+        var diff = Math.abs(medicineTime.diff(patientMedicineTime, 'seconds'));
+        if (diff <= 3600) {
+            this.emit(':ask', this.t('MEDICINE_TAKEN'));
         } else {
-            this.emit(':ask' , "This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?");
+            this.emit(':ask', this.t('NOT_TAKEN_MEDICINE'));
         }
-        
+
+    },
+    'VisitDoneIntent': function () {
+        var visitTime = this.event.request.intent.slots.visitTime.value;
+        this.emit(':ask', visitTime);
+
+    },
+    'VisitStartIntent': function () {
+        this.emit(':ask', 'VisitStartIntent');
+
+    },
+    'PainDefinedIntent': function () {
+        var bodyPart = this.event.request.intent.slots.bodyPart.value;
+        this.emit(':ask', bodyPart);
+
+    },
+    'PainNotDefinedIntent': function () {
+        this.emit(':ask', 'PainNotDefinedIntent');
+
+    },
+    'PainIntesityIntent': function () {
+        var painIntesity = this.event.request.intent.slots.painIntesity.value;
+        this.emit(':ask', painIntesity);
+
+    },
+    'PainFrequencyIntent': function () {
+        var painFrequency = this.event.request.intent.slots.painFrequency.value;
+        this.emit(':ask', painFrequency);
+
+    },
+    'ToiletTypeIntent': function () {
+        var stoodType = this.event.request.intent.slots.stoodType.value;
+        this.emit(':ask', stoodType);
+
+    },
+    'EatIntent': function () {
+        this.emit(':ask', 'EatIntent');
+
+    },
+    'FeelGoodIntent': function () {
+        this.emit(':ask', 'FeelGoodIntent');
+
     },
     'AMAZON.HelpIntent': function () {
-        this.emit(':ask', 'Help', 'Help');
+        this.emit(':ask', this.t('HELP_MESSAGE'), this.t('HELP_MESSAGE'));
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Okay, good bye!');
+        this.emit(':tell', this.t('GOOD_BYE'));
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Okay, good bye!');
+        this.emit(':tell', this.t('GOOD_BYE'));
     },
     'SessionEndedRequest': function () {
-        this.emit(':tell', 'Okay, good bye!');
+        this.emit(':tell', this.t('GOOD_BYE'));
     },
 };
 
@@ -75,6 +145,7 @@ exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
