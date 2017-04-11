f97d475077f7dcc4fd1d011e3d28229733258652
zolikobol <zoltan.kobol@globallogic.com>
2017-04-11 18:58:51 +0200

    defined PainIntensityIntent

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mDoc-alexa
+
+Alexa mDoc skill to communicate with patients. This is property of mDoc, do not use it!
--- index.js
+++ index.js
@@ -9,9 +9,17 @@ const moment = require('moment');
 
 const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
-var dummyValues = {
-    'medicineTime': '09:00'
+var predefinedValues = {
+    'medicineTime': '09:00',
+    'painIntensityLight' : [
+        'light'
+    ],
+    'painIntensityHeavy' : [
+        'heavy'
+    ],
 }
+
+var colectedData = {};
 
 var languageStrings = {
     'en-GB': {
@@ -21,7 +29,13 @@ var languageStrings = {
             'NOT_TAKEN_MEDICINE': 'This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?',
             'MEDICINE_TAKEN': 'Good to hear. You have to attend doctors visit. When did you attend the visit?',
             'HELP_MESSAGE': 'Help!',
-            'GOOD_BYE': 'Good bye!'
+            'GOOD_BYE': 'Good bye!',
+            'VISIT_START_MESSAGE' : 'Okay then. We will make today visit now. Do you feel good or you feel pain?',
+            'PAIN_NOT_DEFINED' : 'That is not good. Where do you feel pain? In head, in arms, in legs or in stomach?',
+            'PAIN_DEFINED' : 'Pain in BODY_PART is not good. Is it heavy pain or light pain?',
+            'HEAVY_PAIN_MESSAGE' : 'Heavy pain is dangerous. I will allert your doctor.',
+            'LIGHT_PAIN_MESSAGE' : 'Do you feel it on regular basis or just now?',
+            'UNHANDLED' : 'Sorry I didnt quite get that!'
         }
     },
     'en-US': {
@@ -31,7 +45,13 @@ var languageStrings = {
             'NOT_TAKEN_MEDICINE': 'This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?',
             'MEDICINE_TAKEN': 'Good to hear. You have to attend doctors visit. When did you attend the visit?',
             'HELP_MESSAGE': 'Help!',
-            'GOOD_BYE': 'Good bye!'
+            'GOOD_BYE': 'Good bye!',
+            'VISIT_START_MESSAGE' : 'Okay then. We will make today visit now. Do you feel good or you feel pain?',
+            'PAIN_NOT_DEFINED' : 'That is not good. Where do you feel pain? In head, in arms, in legs or in stomach?',
+            'PAIN_DEFINED' : 'Pain in BODY_PART is not good. Is it heavy pain or light pain?',
+            'HEAVY_PAIN_MESSAGE' : 'Heavy pain is dangerous. I will allert your doctor.',
+            'LIGHT_PAIN_MESSAGE' : 'Do you feel it on regular basis or just now?',
+            'UNHANDLED' : 'Sorry I didnt quite get that!'
         }
     },
     'de-DE': {
@@ -41,15 +61,27 @@ var languageStrings = {
             'NOT_TAKEN_MEDICINE': 'This is not good. You need to take your medicine every day at 9 AM.You have to attend doctors visit. When did you attend the visit?',
             'MEDICINE_TAKEN': 'Good to hear. You have to attend doctors visit. When did you attend the visit?',
             'HELP_MESSAGE': 'Help!',
-            'GOOD_BYE': 'Good bye!'
+            'GOOD_BYE': 'Good bye!',
+            'VISIT_START_MESSAGE' : 'Okay then. We will make today visit now. Do you feel good or you feel pain?',
+            'PAIN_NOT_DEFINED' : 'That is not good. Where do you feel pain? In head, in arms, in legs or in stomach?',
+            'PAIN_DEFINED' : 'Pain in BODY_PART is not good. Is it heavy pain or light pain?',
+            'HEAVY_PAIN_MESSAGE' : 'Heavy pain is dangerous. I will allert your doctor.',
+            'LIGHT_PAIN_MESSAGE' : 'Do you feel it on regular basis or just now?',
+            'UNHANDLED' : 'Sorry I didnt quite get that!'
         }
     }
 };
 
 const handlers = {
+    /*
+     *  LaunchRequest is the first fired when we start the skill with "Alxa open the doctor""
+     */
     'LaunchRequest': function () {
         this.emit(':ask', this.t('WELCOME_MESSAGE'));
     },
+    /*
+     *  PatientDefinedIntent is firing when we say our name
+     */
     'PatientDefineIntent': function () {
         if (typeof this.attributes['mdoc'] == 'undefined') {
             var attributes = this.attributes['mdoc'];
@@ -65,14 +97,19 @@ const handlers = {
         this.attributes['mdoc'] = attributes;
         this.emit(':ask', "Hello " + firstName + " " + lastName + ". " + this.t('MEDICINE_OFFER'), this.t('MEDICINE_OFFER'));
     },
+    /*
+     *
+     */
     'MedicineIntent': function () {
         var attributes = this.event.session.attributes;
         var patientMedicineTime = moment(this.event.request.intent.slots.medicineTime.value, 'HH:mm');
         if (typeof patientMedicineTime == 'undefined') {
+            colectedData['medicineTime'] = false;
             this.emit(':ask', this.t('NOT_TAKEN_MEDICINE'));
         }
-        var medicineTime = moment(dummyValues['medicineTime'], 'HH:mm');
+        var medicineTime = moment(predefinedValues['medicineTime'], 'HH:mm');
         var diff = Math.abs(medicineTime.diff(patientMedicineTime, 'seconds'));
+        colectedData['medicineTime'] = medicineTime;
         if (diff <= 3600) {
             this.emit(':ask', this.t('MEDICINE_TAKEN'));
         } else {
@@ -80,56 +117,106 @@ const handlers = {
         }
 
     },
+    /*
+     *
+     */
     'VisitDoneIntent': function () {
         var visitTime = this.event.request.intent.slots.visitTime.value;
+        colectedData['visitTime'] = visitTime;
         this.emit(':ask', visitTime);
-
     },
+    /*
+     *
+     */
     'VisitStartIntent': function () {
-        this.emit(':ask', 'VisitStartIntent');
+        this.emit(':ask', this.t('VISIT_START_MESSAGE'));
 
     },
+    /*
+     *
+     */
     'PainDefinedIntent': function () {
         var bodyPart = this.event.request.intent.slots.bodyPart.value;
-        this.emit(':ask', bodyPart);
+        colectedData['pain'] = bodyPart;
+        var message = this.t('PAIN_DEFINED').replace('BODY_PART' , bodyPart);
+        this.emit(':ask', message);
 
     },
+    /*
+     *
+     */
     'PainNotDefinedIntent': function () {
-        this.emit(':ask', 'PainNotDefinedIntent');
+        this.emit(':ask', this.t('PAIN_NOT_DEFINED'));
 
     },
+    /*
+     *
+     */
     'PainIntesityIntent': function () {
         var painIntesity = this.event.request.intent.slots.painIntesity.value;
-        this.emit(':ask', painIntesity);
-
+        var lightPain = predefinedValues.painIntensityLight;
+        var heavyPain = predefinedValues.painIntensityHeavy;
+        if(lightPain.indexOf(painIntesity) >= 0){
+            colectedData['painIntensity'] = 1;
+            this.emit(':ask', this.t('LIGHT_PAIN_MESSAGE'));
+        }
+        if(heavyPain.indexOf(painIntesity) >= 0){
+            colectedData['painIntensity'] = 2;
+            this.emit(':tell', this.t('HEAVY_PAIN_MESSAGE'));
+        }
+        this.emit(':ask', this.t('UNHANDLED'));
     },
+    /*
+     *
+     */
     'PainFrequencyIntent': function () {
         var painFrequency = this.event.request.intent.slots.painFrequency.value;
         this.emit(':ask', painFrequency);
 
     },
+    /*
+     *
+     */
     'ToiletTypeIntent': function () {
         var stoodType = this.event.request.intent.slots.stoodType.value;
         this.emit(':ask', stoodType);
 
     },
+    /*
+     *
+     */
     'EatIntent': function () {
         this.emit(':ask', 'EatIntent');
 
     },
+    /*
+     *
+     */
     'FeelGoodIntent': function () {
         this.emit(':ask', 'FeelGoodIntent');
 
     },
+    /*
+     *
+     */
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', this.t('HELP_MESSAGE'), this.t('HELP_MESSAGE'));
     },
+    /*
+     *
+     */
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', this.t('GOOD_BYE'));
     },
+    /*
+     *
+     */
     'AMAZON.StopIntent': function () {
         this.emit(':tell', this.t('GOOD_BYE'));
     },
+    /*
+     *
+     */
     'SessionEndedRequest': function () {
         this.emit(':tell', this.t('GOOD_BYE'));
     },
